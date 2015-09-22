43966d0058202fea73d801a7457159d8f0829b49
Hector <hector.cadavid@gmail.com>
2015-09-22 13:24:31 -0500

    first revision

--- src/main/java/edu/eci/pdsw/samples/textview/MyBatisExample.java
+++ src/main/java/edu/eci/pdsw/samples/textview/MyBatisExample.java
@@ -16,11 +16,9 @@
  */
 package edu.eci.pdsw.samples.textview;
 
-import edu.eci.pdsw.samples.mybatis.mappers.ProductoMapper;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.SQLException;
-import java.util.LinkedList;
 import java.util.List;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -34,8 +32,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 public class MyBatisExample {
 
     /**
-     * Método que construye una fábrica de sesiones de MyBatis a partir
-     * del archivo de configuración ubicado en src/main/resources
+     * Método que construye una fábrica de sesiones de MyBatis a partir del
+     * archivo de configuración ubicado en src/main/resources
+     *
      * @return instancia de SQLSessionFactory
      */
     public static SqlSessionFactory getSqlSessionFactory() {
@@ -52,80 +51,94 @@ public class MyBatisExample {
         return sqlSessionFactory;
     }
 
-    public static void main(String args[]) {
-        SqlSessionFactory sessionfact=getSqlSessionFactory();
-        
-        SqlSession sqlss=sessionfact.openSession();
-        
-        ProductoMapper pmap=sqlss.getMapper(ProductoMapper.class);
+    /**
+     * Programa principal de ejempo de uso de MyBATIS
+     * @param args
+     * @throws SQLException 
+     */
+    public static void main(String args[]) throws SQLException {
+        SqlSessionFactory sessionfact = getSqlSessionFactory();
 
-        
-        
-        
+        SqlSession sqlss = sessionfact.openSession();
+
+        System.out.println("Valor total pedido 1:" + valorTotalPedido(sqlss, 1));
+
+        List<String> prodsPedido = nombresProductosPedido(sqlss, 1);
+
+        System.out.println("Productos del pedido 1:");
+        System.out.println("-----------------------");
+        for (String nomprod : prodsPedido) {
+            System.out.println(nomprod);
+        }
+        System.out.println("-----------------------");
+
+        int suCodigoECI = 20134423;
+        registrarNuevoProducto(sqlss, suCodigoECI, "SU NOMBRE", 99999999);
+        sqlss.commit();
+
+        cambiarNombreProducto(sqlss, suCodigoECI, "EL NUEVO NOMBRE");
+        sqlss.commit();
+
+        sqlss.close();
+
     }
-    
+
     /**
      * Agregar un nuevo producto con los parámetros dados
+     *
      * @param con la conexión JDBC
      * @param codigo
      * @param nombre
      * @param precio
-     * @throws SQLException 
+     * @throws SQLException
      */
-    public static void registrarNuevoProducto(SqlSession session, int codigo, String nombre,int precio) throws SQLException{
-        
+    public static void registrarNuevoProducto(SqlSession session, int codigo, String nombre, int precio) throws SQLException {
+
         //Obtener el 'mapper' correspondiente
         //Invocar a la función del mapper
-        
     }
-    
+
     /**
      * Consultar los nombres de los productos asociados a un pedido
+     *
      * @param con la conexión JDBC
      * @param codigoPedido el código del pedido
-     * @return 
+     * @return
      */
-    public static List<String> nombresProductosPedido(SqlSession session, int codigoPedido){
-        List<String> np=null;
-        
+    public static List<String> nombresProductosPedido(SqlSession session, int codigoPedido) {
+        List<String> np = null;
+
         //Obtener el 'mapper' correspondiente
         //Invocar a la función del mapper
-        
         return np;
     }
 
-    
     /**
      * Calcular el costo total de un pedido
+     *
      * @param con
      * @param codigoPedido código del pedido cuyo total se calculará
      * @return el costo total del pedido (suma de: cantidades*precios)
      */
-    public static int valorTotalPedido(SqlSession session, int codigoPedido){
-        
+    public static int valorTotalPedido(SqlSession session, int codigoPedido) {
+
         //Obtener el 'mapper' correspondiente
         //Invocar a la función del mapper
-        
         return 0;
     }
-    
 
     /**
      * Cambiar el nombre de un producto
+     *
      * @param con
      * @param codigoProducto codigo del producto cuyo nombre se cambiará
      * @param nuevoNombre el nuevo nombre a ser asignado
      */
-    public static void cambiarNombreProducto(SqlSession session, int codigoProducto, 
-            String nuevoNombre){
-        
+    public static void cambiarNombreProducto(SqlSession session, int codigoProducto,
+            String nuevoNombre) {
+
         //Obtener el 'mapper' correspondiente
         //Invocar a la función del mapper
-        
-        
     }
-    
-    
-    
-    
+
 }
