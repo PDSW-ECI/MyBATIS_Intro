5c5bb686aedbec2792a873607cac0f486c1481be
Hector <hector.cadavid@gmail.com>
2015-09-22 11:56:16 -0500

    first revision

---
+++ src/main/java/edu/eci/pdsw/samples/textview/MyBatisExample.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2015 hcadavid
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package edu.eci.pdsw.samples.textview;
+
+import edu.eci.pdsw.samples.mybatis.mappers.ProductoMapper;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class MyBatisExample {
+
+    /**
+     * Método que construye una fábrica de sesiones de MyBatis a partir
+     * del archivo de configuración ubicado en src/main/resources
+     * @return instancia de SQLSessionFactory
+     */
+    public static SqlSessionFactory getSqlSessionFactory() {
+        SqlSessionFactory sqlSessionFactory = null;
+        if (sqlSessionFactory == null) {
+            InputStream inputStream;
+            try {
+                inputStream = Resources.getResourceAsStream("mybatis-config.xml");
+                sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e.getCause());
+            }
+        }
+        return sqlSessionFactory;
+    }
+
+    public static void main(String args[]) {
+        SqlSessionFactory sessionfact=getSqlSessionFactory();
+        
+        SqlSession sqlss=sessionfact.openSession();
+        
+        ProductoMapper pmap=sqlss.getMapper(ProductoMapper.class);
+        System.out.println(pmap.selectProduct(1));
+        System.out.println(pmap.selectAllProducts());
+        
+    }
+    
+    /**
+     * Agregar un nuevo producto con los parámetros dados
+     * @param con la conexión JDBC
+     * @param codigo
+     * @param nombre
+     * @param precio
+     * @throws SQLException 
+     */
+    public static void registrarNuevoProducto(SqlSession session, int codigo, String nombre,int precio) throws SQLException{
+        //Crear preparedStatement
+        //Asignar parámetros
+        //usar 'execute'
+        
+    }
+    
+    /**
+     * Consultar los nombres de los productos asociados a un pedido
+     * @param con la conexión JDBC
+     * @param codigoPedido el código del pedido
+     * @return 
+     */
+    public static List<String> nombresProductosPedido(SqlSession session, int codigoPedido){
+        List<String> np=new LinkedList<>();
+        
+        //Crear prepared statement
+        //asignar parámetros
+        //usar executeQuery
+        //Sacar resultados del ResultSet
+        //Llenar la lista y retornarla
+        
+        return np;
+    }
+
+    
+    /**
+     * Calcular el costo total de un pedido
+     * @param con
+     * @param codigoPedido código del pedido cuyo total se calculará
+     * @return el costo total del pedido (suma de: cantidades*precios)
+     */
+    public static int valorTotalPedido(SqlSession session, int codigoPedido){
+        
+        //Crear prepared statement
+        //asignar parámetros
+        //usar executeQuery
+        //Sacar resultado del ResultSet
+        
+        return 0;
+    }
+    
+
+    /**
+     * Cambiar el nombre de un producto
+     * @param con
+     * @param codigoProducto codigo del producto cuyo nombre se cambiará
+     * @param nuevoNombre el nuevo nombre a ser asignado
+     */
+    public static void cambiarNombreProducto(SqlSession session, int codigoProducto, 
+            String nuevoNombre){
+        
+        //Crear prepared statement
+        //asignar parámetros
+        //usar executeUpdate
+        //verificar que se haya actualizado exactamente un registro
+        
+        
+    }
+    
+    
+    
+    
+}
